b8eb1d47a24ced49885d1d7d3c37d197965e5d31
Ariful Islam <66293325+aifulislam@users.noreply.github.com>
2020-07-23 10:56:05 +0600

    C_Coding

--- file.c
+++ file.c
@@ -1,39 +1,15 @@
 #include<stdio.h>
 int main()
 {
-//228-File-Introduction to File
-    FILE *file;
-    file = fopen("test.txt","w");
-    if(file==NULL)
-    {
-        printf("File dosen't exist.");
-    }
-    else
-    {
-        printf("File is opened.");
-        fclose(file);
-    }
-//228-File-Introduction to File
-    FILE *file1;
-    file1 = fopen("test.txt","w");
-    if(file1==NULL)
-    {
-        printf("File1 dosen't exist.");
-    }
-    else
-    {
-        printf("File1 is opened.");
-        fclose(file1);
-    }
 //229-File-Writing to a file using fputc()
-    FILE  *file;
-    char name[20]="Ariful Islam";
+    FILE *file;
+    char name[20]= "Ariful Islam";
     int length = strlen(name);
     int i;
     file = fopen("test.txt","w");
-    if(file== NULL)
+    if(file==NULL)
     {
-        printf("File dosen't esist.");
+        printf("File doesn't esist.");
     }
     else
     {
@@ -42,10 +18,74 @@ int main()
         {
             fputc(name[i],file);
         }
-        printf("File2 is written successfully.");
+        //printf("File is written successfully.\n");
+        fclose(file);
+    }
+//230-File-Writing to a file using fputs()
+    FILE *file;
+    char name[20];
+    file = fopen("test.txt","w");
+    if(file==NULL)
+    {
+        printf("File doesn't esist.");
+    }
+    else
+    {
+        printf("File is opened. \n");
+        printf("Enter your college name.");
+        gets(name);
+        fputc(name,file);
+        printf("File is written successfully. \n");
+
+
         fclose(file);
     }
 
 
-getch();
+//231-File-Writing to a file using fprintf()
+    FILE *file;
+    char name[30];
+    int age;
+    file = fopen("test.txt","a");
+    if(file==NULL)
+    {
+        printf("File doesn't esist.");
+    }
+    else
+    {
+        printf("File is opened. \n");
+        printf("Enter your name: ");
+        gets(name);
+        printf("Enter your age: ");
+        scanf("%d",&age);
+        fprintf(file,"Name = %s, Age = %d\n",name,age);
+        printf("File is written successfully. \n");
+        fclose(file);
+    }
+
+
+//232 - File - Reading a file using fgetc()
+    FILE *file;
+    char ch;
+    file = fopen("test.txt","r");
+    if(file==NULL)
+    {
+        printf("File dosn't exist.\n");
+    }
+    else
+    {
+        printf("File is opened.\n");
+        char ch=fgetc(file);
+        while(!feof(file))
+        {
+            ch= fgetc(file);
+            printf("%c",ch);
+        }
+
+
+        fclose(file);
+    }
+
+
+    getch();
 }
